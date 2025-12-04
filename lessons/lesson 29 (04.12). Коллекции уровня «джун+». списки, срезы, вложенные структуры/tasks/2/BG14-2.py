6eea58b1f2543a933ee886ccb7571bb13851a9bb
BykovaTanya <stervod4@mail.ru>
2025-12-04 21:17:10 +0500

    Add files via upload

---
+++ lessons/lesson 29 (04.12). Коллекции уровня «джун+». списки, срезы, вложенные структуры/tasks/2/BG14-2.py
@@ -0,0 +1,6 @@
+1	time_slots = ["08:00", "09:30", "11:00", "12:30", "14:00", "15:30", "17:00", "18:30", "20:00"]
+2	
+3	print(time_slots[:4]) 
+4	print(time_slots[-3:]) 
+5	print(time_slots[1:6]) 
+6	print(time_slots[::2])
