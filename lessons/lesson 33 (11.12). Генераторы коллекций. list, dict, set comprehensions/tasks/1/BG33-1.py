f5d235445ab3ef6c8587d971ade4d8efbd56120f
BykovaTanya <BagiraLada@yandex.ru>
2025-12-11 19:39:38 +0500

    Add files via upload

---
+++ lessons/lesson 33 (11.12). Генераторы коллекций. list, dict, set comprehensions/tasks/1/BG33-1.py
@@ -0,0 +1,10 @@
+
+scores = [5, 9, 10, 7, 8, 6]
+new_scores = []
+for score in scores:
+    if score >= 8:
+    new_scores.append(scores)
+print(new_scores)
+for index, score_name in enumerate(new_scores, start=1):
+    print(f"Студент №{index}: {score_name}")
+
