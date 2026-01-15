4d4c472ae6af1375a5976b2fc772e4feea566779
BykovaTanya <BagiraLada@yandex.ru>
2026-01-15 20:38:42 +0500

    Delete lessons/lesson 45 (15.01). Работа с файлами/tasks/BG45.py

--- lessons/lesson 45 (15.01). Работа с файлами/tasks/BG45.py
+++
@@ -1,6 +0,0 @@
-with open("BG45.txt", "r", encoding="utf-8") as file:
-    #content = file.read()
-    for line in file:
-        if  line.strip():
-            print(line.strip())
-    #print(file)
